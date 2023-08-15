60e6db6a36e9bfe1d2b92ee9eb4027e7c867f20c
arlesson menezes <arlesson.a7x@gmail.com>
2023-08-14 21:36:42 -0300

    started project

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    } as TypeOrmModuleOptions)
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
